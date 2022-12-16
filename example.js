e18ba9864af9ffc716a26c51652f6b557ed426a9
Emre KETENCİ <75275978+emreketenci@users.noreply.github.com>
2022-12-16 15:42:53 +1100

    Update example.js

--- example.js
+++ example.js
@@ -24,3 +24,59 @@ client.on('guildMemberRemove', member => {
 
 // Botunuzun tokenini değiştirin
 client.login('BOT_TOKEN');
+
+
+
+
+
+
+////////////////////////////
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', message => {
+  if (message.content.startsWith('/mute')) {
+    // Kullanıcıyı muteleyecek kod buraya yazılacak
+    const user = message.mentions.users.first();
+    const muteRole = message.guild.roles.cache.find(role => role.name === 'Muted');
+    
+    if (!muteRole) {
+      // Muted rolü yoksa oluştur
+      message.guild.roles.create({
+        data: {
+          name: 'Muted',
+          permissions: [],
+        },
+      })
+      .then(role => console.log(`Created new role with name ${role.name}`))
+      .catch(console.error);
+    }
+    
+    if (user) {
+      const member = message.guild.member(user);
+      if (member) {
+        member.roles.add(muteRole)
+          .then(() => {
+            message.channel.send(`${user.tag} seste mute edildi.`);
+            // Kullanıcıyı 30 dakika sonra unmute et
+            setTimeout(() => {
+              member.roles.remove(muteRole);
+              message.channel.send(`${user.tag} seste unmute edildi.`);
+            }, 1800000);
+          })
+          .catch(console.error);
+      } else {
+        message.channel.send('Bu kullanıcı sunucuda değil.');
+      }
+    } else {
+      message.channel.send('Lütfen mute edilecek bir kullanıcı belirtin.');
+    }
+  }
+});
+
+client.login('Your_Token_Here');
+
